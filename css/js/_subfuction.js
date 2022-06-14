58d84b0dc05bd9813761219e72f22976a9f4a564
thivuu <105454598+thivuu@users.noreply.github.com>
2022-06-14 16:29:47 +0700

    add new file
    ad new file

---
+++ css/js/_subfuction.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".file").click(function(){
+      $("#fileopen").toggle();
+    });
+    // $("body").click(function(){
+    //   $("#fileopen").hide();
+    // });
+    $(".liright").hover(function(){
+      $(".show").show();
+    });
+  });
+    
+  
